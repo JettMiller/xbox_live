cd299c915ca1547856a5e905a1eb29997320e978
JettMiller <jett.miller@perficient.com>
2022-01-28 15:25:50 -0600

    fixes

--- src/test/java/xbox/testcases/XboxVerify.java
+++ src/test/java/xbox/testcases/XboxVerify.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import util.CustomAssertion;
 import util.TestCaseBase;
@@ -20,6 +21,7 @@ public class XboxVerify extends TestCaseBase {
 		XboxHomePage home = new XboxHomePage();
 		home.open();
 		home.waitPage();
+		SoftAssert soft = new SoftAssert();
 		
 		String user1;
 		String user2;
@@ -41,16 +43,19 @@ public class XboxVerify extends TestCaseBase {
 		WebElement top3 = driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div[3]/a"));
 		WebElement top4 = driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div/div[3]/a"));
 		WebElement top5 = driver.findElement(By.xpath("/html/body/div[1]/div/div[5]/div/div[3]/a"));
-		Assert.assertEquals(top1.getText(), user1);
-		Assert.assertEquals(top2.getText(), user2);
-		Assert.assertEquals(top3.getText(), user3);
-		Assert.assertEquals(top4.getText(), user4);
-		Assert.assertEquals(top5.getText(), user5);
+		soft.assertEquals(top1.getText(), user1);
+		soft.assertEquals(top2.getText(), user2);
+		soft.assertEquals(top3.getText(), user3);
+		soft.assertEquals(top4.getText(), user4);
+		soft.assertEquals(top5.getText(), user5);
+		
+		soft.assertAll();
 	}
 	@Test(groups = {"ChromeWin32", "IEWin32"})
 	public void gtsearch() throws InterruptedException, FileNotFoundException {
 		XboxHomePage home = new XboxHomePage();
 		CustomAssertion assertion = new CustomAssertion();
+		SoftAssert soft = new SoftAssert();
 		home.open();
 		home.waitPage();
 		String user;
@@ -79,9 +84,11 @@ public class XboxVerify extends TestCaseBase {
 		WebElement rgame2 = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[2]/div/div/div[1]/h3/a"));
 		WebElement rgame3 = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/div[1]/h3/a"));
 		WebElement rgame4 = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[4]/div/div/div[1]/h3/a"));
-		Assert.assertEquals(rgame1.getText(), game1);
-		Assert.assertEquals(rgame2.getText(), game2);
-		Assert.assertEquals(rgame3.getText(), game3);
-		Assert.assertEquals(rgame4.getText(), game4);
+		soft.assertEquals(rgame1.getText(), game1);
+		soft.assertEquals(rgame2.getText(), game2);
+		soft.assertEquals(rgame3.getText(), game3);
+		soft.assertEquals(rgame4.getText(), game4);
+		
+		soft.assertAll();
 	}
 }
