ea4f3d77cb2686a3736cac943161112159299409
JettMiller <jett.miller@perficient.com>
2022-01-28 11:25:06 -0600

    Corrected Tests

--- src/test/java/xbox/testcases/XboxVerify.java
+++ src/test/java/xbox/testcases/XboxVerify.java
@@ -14,26 +14,7 @@ import util.TestCaseBase;
 import xbox.basepages.XboxHomePage;
 
 public class XboxVerify extends TestCaseBase {
-
-	@Test(groups = {"ChromeWin32", "IEWin32"})
-	public void open() throws InterruptedException {
-		XboxHomePage home = new XboxHomePage();
-		CustomAssertion assertion = new CustomAssertion();
-		home.open();
-		home.waitPage();
-		Assert.assertTrue(home.titleIs(home.TITLE));
-		
-		WebElement top1 = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[3]/a"));
-		WebElement top2 = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div[3]/a"));
-		WebElement top3 = driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div[3]/a"));
-		WebElement top4 = driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div/div[3]/a"));
-		WebElement top5 = driver.findElement(By.xpath("/html/body/div[1]/div/div[5]/div/div[3]/a"));
-		Assert.assertEquals(top1.getText(), "fumingpuma5803");
-		Assert.assertEquals(top2.getText(), "Chrissie");
-		Assert.assertEquals(top3.getText(), "WNA3T");
-		Assert.assertEquals(top4.getText(), "Senjuu x");
-		Assert.assertEquals(top5.getText(), "SpriTe Iight");
-	}
+	
 	@Test(groups = {"ChromeWin32", "IEWin32"})
 	public void top5gt() throws InterruptedException, FileNotFoundException {
 		XboxHomePage home = new XboxHomePage();
